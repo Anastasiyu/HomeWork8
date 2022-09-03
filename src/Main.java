9c18ded116760bcf86ada613b6a0742da91c10fa
Anastasiyu <asy.safiulina@mail.ru>
2022-09-03 21:44:32 +0300

    ДЗ задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        leapYear(2021);
+    }
+    public static void leapYear (int year) {
+
+        if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
     }
 }
