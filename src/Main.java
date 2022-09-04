74b1465b0464ae2a8fc502fc979f92275ff2480f
Anastasiyu <asy.safiulina@mail.ru>
2022-09-04 16:44:07 +0300

    ДЗ задача 2 корректировка

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task2();
     }
 
     public static void task1() {
@@ -16,4 +17,30 @@ public class Main {
         } else {
             System.out.println(year + " - не високосный год");
         }
-    }}
+    }
+    public static void desiredLink (int clientOS, int clientDeviceYear) {
+
+
+        if (clientOS == 0) {
+
+            if (clientDeviceYear < 2015) {
+
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+    }
+        public static void task2() {
+            int clientOS = 0;
+            int clientDeviceYear = LocalDate.now().getYear();
+            desiredLink(clientOS, clientDeviceYear);
+        }
+
+    }
